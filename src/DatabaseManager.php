6db32fd0319428d73ce0056f63565acebd372d1f
Luke Lanchester <luke@lukelanchester.com>
2015-10-03 13:06:51 +0100

    Added convenience methods for raw queries to manager

--- src/DatabaseManager.php
+++ src/DatabaseManager.php
@@ -52,4 +52,83 @@ class DatabaseManager {
 
 
 
+
+
+
+	/**
+	 * Perform a raw query
+	 *
+	 * @param string SQL
+	 * @param array Query params
+	 * @return bool Result
+	 **/
+	public function queryRaw($sql, array $params = null) {
+
+		return $this->connection()->query($sql, $params);
+
+	}
+
+
+
+	/**
+	 * Perform a raw select query
+	 *
+	 * @param string SQL
+	 * @param array Query params
+	 * @return array Rows
+	 **/
+	public function selectRaw($sql, array $params = null) {
+
+		return $this->connection()->select($sql, $params);
+
+	}
+
+
+
+	/**
+	 * Perform a raw insert query
+	 *
+	 * @param string SQL
+	 * @param array Query params
+	 * @param bool Return insert ID
+	 * @return array|false [int Rows affected, int Insert ID]
+	 **/
+	public function insertRaw($sql, array $params = null, $fetch_insert_id = true) {
+
+		return $this->connection()->insert($sql, $params, $fetch_insert_id);
+
+	}
+
+
+
+	/**
+	 * Perform a raw update query
+	 *
+	 * @param string SQL
+	 * @param array Query params
+	 * @return int|false Rows affected
+	 **/
+	public function updateRaw($sql, array $params = null) {
+
+		return $this->connection()->update($sql, $params);
+
+	}
+
+
+
+	/**
+	 * Perform a raw delete query
+	 *
+	 * @param string SQL
+	 * @param array Query params
+	 * @return int|false Rows affected
+	 **/
+	public function deleteRaw($sql, array $params = null) {
+
+		return $this->connection()->delete($sql, $params);
+
+	}
+
+
+
 }
